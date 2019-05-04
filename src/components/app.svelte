877f16756fb38e837be13553ecdcf85ac8e5ce7e
Bernardo A. Dal Corno <bernardoadc@gmail.com>
2019-05-04 03:40:29 -0300

    esqueleto inicial

--- src/components/app.svelte
+++ src/components/app.svelte
@@ -1,16 +1,5 @@
-<svelte:head>
-  <!-- {1:head content} -->
-  <link rel='stylesheet' href='main.css'>
-</svelte:head>
+<script>
+</script>
 
 <style>
-
 </style>
-
-<!-- <template> -->
-  <h1>Hello {name}!!!</h1>
-<!-- </template> -->
-
-<script>
-	export let name
-</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,7 @@
 import App from './components/app.svelte'
 
-const app = new App({
+export default new App({
 	target: document.body,
 	props: {
-		name: 'UNIVERSE'
 	}
 })
-
-export default app
