ff1d27d004eae30221558fc146b30ebe58630fe5
Bernardo A. Dal Corno <bernardoadc@gmail.com>
2019-05-28 03:09:04 -0300

    botao flip bonito

--- src/app.svelte
+++ src/app.svelte
@@ -1,7 +1,12 @@
-<button id="orientation" on:click={toggleOrientation}>
-  flip flop
+<button id="device" on:click={toggleDevice}>
+  <img src="/resources/icons/devices.png" alt="change device size">
+  <span>{device}</span>
 </button>
-<div id="app" class:mobile class:tablet class={orientation}>
+<button id="orientation" on:click={toggleOrientation}>
+  <img src="/resources/icons/flip.png" alt="flip orientation">
+  <!-- <span>{orientation}</span> -->
+</button>
+<div id="app" class="{device} {orientation}">
 </div>
 
 
@@ -11,7 +16,7 @@
   :global(body) {
     padding: 0px !important;
     font-family: 'Open Sans', sans-serif;
-    background-color: #f1f1f1;
+    background-color: #c0c0c0;
     width: 100vw;
     height: 100vh;
   }
@@ -20,10 +25,35 @@
     overflow-y: hidden;
   }
 
-  #orientation {
+  #device, #orientation {
     position: absolute;
-    top: 10vh;
-    left: 10vw;
+    top: 23px;
+    right: 23px;
+    border-radius: 10px;
+    padding: 10px;
+    /* background-color: #f1f1f1; */
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  }
+
+  #device {
+    top: 23px;
+    left: 23px;
+  }
+
+  #orientation {
+    top: 23px;
+    right: 23px;
+  }
+
+  #device > img, #orientation > img {
+    width: 32px;
+    height: 32px;
+  }
+
+  #device > span, #orientation > span {
+    display: block;
+    font-size: smaller;
+    color: grey;
   }
 
 /*** W3Schools https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_device_smartphone ***/
@@ -96,8 +126,10 @@
 
 /* The device with borders */
 .tablet {
-  position: relative;
-  margin: auto;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   border: 16px black solid;
   border-radius: 36px;
 }
@@ -164,9 +196,15 @@
 
 <script>
   //props
-  let mobile = true
-  let tablet = false
+  let device = 'mobile'
   let orientation = 'portrait'
+
+  function toggleDevice () {
+    if (device == 'mobile')
+      device = 'tablet'
+    else
+      device = 'mobile'
+  }
 
   function toggleOrientation () {
     if (orientation == 'portrait')
